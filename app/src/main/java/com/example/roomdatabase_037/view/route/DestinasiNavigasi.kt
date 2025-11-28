926efcc55c8f40946779c742e76da72710d55ba2
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 17:17:39 +0700

    mendefinisikan sebuah interface bernama DestinasiNavigasi

---
+++ app/src/main/java/com/example/roomdatabase_037/view/route/DestinasiNavigasi.kt
@@ -0,0 +1,13 @@
+package com.example.roomdatabase_037.view.route
+
+interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+
+    /**
+     *
+     */
+    val titleRes: Int
+}
