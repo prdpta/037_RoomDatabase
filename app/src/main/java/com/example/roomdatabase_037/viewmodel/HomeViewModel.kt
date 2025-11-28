fe35c3c4ae7ce2d905d33b77ed0ddb622f8356bc
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 20:01:01 +0700

    mengambil dan menyediakan data daftar siswa

---
+++ app/src/main/java/com/example/roomdatabase_037/viewmodel/HomeViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.roomdatabase_037.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase_037.repositori.RepositoriSiswa
+import com.example.roomdatabase_037.room.Siswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel()
+    {
+        companion object {
+            private const val TIMEOUT_MILLIS = 5_000L
+        }
+
+
