5713aac204b526bf027d0dedc0beb788a12ac72c
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 20:05:22 +0700

    mengelola dan memvalidasi input data siswa

---
+++ app/src/main/java/com/example/roomdatabase_037/viewmodel/EntryViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.roomdatabase_037.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabase_037.repositori.RepositoriSiswa
+import com.example.roomdatabase_037.room.Siswa
+
+class EntryViewModel(
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    /**
+     * Berisi status Siswa saat ini
+     */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi validasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
