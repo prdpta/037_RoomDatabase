4f47be65b957b666ad4afe733b09126484ac24db
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 19:54:43 +0700

    menginisialisasi HomeViewModel dengan menyediakan dependensi RepositoriSiswa

---
+++ app/src/main/java/com/example/roomdatabase_037/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.roomdatabase_037.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase_037.repositori.AplikasiSiswa
+import com.example.roomdatabase_037.viewmodel.EntryViewModel
+import com.example.roomdatabase_037.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
