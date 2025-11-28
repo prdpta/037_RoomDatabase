20cb387df780414810375629508c836b758657d3
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 17:15:00 +0700

    mendefinisikan alamat ("item_entry") dan judul (R.string.entry_siswa)

---
+++ app/src/main/java/com/example/roomdatabase_037/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase_037.view.route
+
+import com.example.roomdatabase_037.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
