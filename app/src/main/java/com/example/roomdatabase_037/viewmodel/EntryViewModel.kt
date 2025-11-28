7e5772baeed9c58aefa5f88cf93a4de87f14be0d
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 20:10:53 +0700

    mendefinisikan sebuah kelas data bernama DetailSiswa

--- app/src/main/java/com/example/roomdatabase_037/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_037/viewmodel/EntryViewModel.kt
@@ -50,3 +50,10 @@ data class UIStateSiswa(
     val isEntryValid: Boolean = false
 )
 
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+
