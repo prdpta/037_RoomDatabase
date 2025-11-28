e5b6ae6969c5dada07457fbe7fa93ce3f662121e
prdpta <pradiptaputramei01@gmail.com>
2025-11-28 20:12:24 +0700

    mendefinisikan sebuah fungsi ekstensi yang bernama toSiswa() pada kelas DetailSiswa

--- app/src/main/java/com/example/roomdatabase_037/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_037/viewmodel/EntryViewModel.kt
@@ -57,3 +57,22 @@ data class DetailSiswa(
     val telpon: String = "",
 )
 
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
+    UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
