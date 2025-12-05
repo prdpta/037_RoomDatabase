9c6ca095dc293ca4f1adeecf7158af86de751dbf
prdpta <pradiptaputramei01@gmail.com>
2025-12-05 15:05:36 +0700

    menghapus data siswa secara langsung di database

--- app/src/main/java/com/example/roomdatabase_037/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_037/repositori/RepositoriSiswa.kt
@@ -20,4 +20,5 @@ class OfflineRepositoriSiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     //edit 2 : tambah 3 override berikut
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
