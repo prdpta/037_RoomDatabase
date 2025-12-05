bb65f7cbb6e45f6c2cbe3773aa213fc2eac3d98c
prdpta <pradiptaputramei01@gmail.com>
2025-12-05 14:59:31 +0700

    mengambil data 1 siswa berdasarkan ID dari database

--- app/src/main/java/com/example/roomdatabase_037/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_037/repositori/RepositoriSiswa.kt
@@ -7,6 +7,8 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
+    //edit 1 : tambah 3 fungsi berikut
+    fun getSiswaStream(id: Int): Flow<Siswa?>
 }
 
 class OfflineRepositoriSiswa(
