6a3c794a81191428dda0072d942251bb60fffb54
prdpta <pradiptaputramei01@gmail.com>
2025-12-05 15:29:14 +0700

    menandai dan membedakan halaman ini dari halaman lain di sistem navigasi

---
+++ app/src/main/java/com/example/roomdatabase_037/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,7 @@
+package com.example.roomdatabase_037.view.route
+
+import com.example.roomdatabase_037.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+}
